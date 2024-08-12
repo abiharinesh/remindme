b13ec6d5e242e31fcd9b8c42963cfa847ec1a575
Gowthaman55 <147603900+Gowthaman55@users.noreply.github.com>
2024-08-12 13:04:55 +0530

    first commit

---
+++ lib/auth/auth_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:remindme/auth/register_screen.dart';
+
+class AuthenticationScreen extends StatefulWidget {
+  const AuthenticationScreen({super.key});
+
+  @override
+  State<AuthenticationScreen> createState() => _AuthenticationScreenState();
+}
+
+class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: RegisterScreen(),
+    );
+  }
+}
